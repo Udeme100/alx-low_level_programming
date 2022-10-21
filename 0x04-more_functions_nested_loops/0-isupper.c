c2b242543d9d932086bae9b6d2c19e5911adbfa4
Udeme100 <udemx200@gmail.com>
2022-10-20 19:14:37 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper -checks if a character is uppercase
+ * _isupper -  checks if a character is uppercase
  * @c: Variable text
  * Return: Always 0
  */
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,7 +2,7 @@
 #include "main.h
 
 /**
- * _putchar - wirtes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return: on success 1 else -1
  */
